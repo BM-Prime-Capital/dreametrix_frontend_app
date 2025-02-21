8bdedfca105e51314cd1cfa40be1449ed0a28fd6
princebak <princebakenga09032017@gmail.com>
2025-02-21 14:28:38 +0100

    patch: All types from types.ts exported

--- components/assignments/AddAssignmentDialog.tsx
+++ components/assignments/AddAssignmentDialog.tsx
@@ -14,6 +14,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { generalImages } from "@/constants/images";
 import TimeTableItem from "../ui/time-table-item";
+import { ClassDay } from "@/types";
 
 export function AddAssignmentDialog() {
   const [open, setOpen] = useState(false);
