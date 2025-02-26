e435f674ec0a377e9d4d9762537d5d4f199bf09d
princebak <princebakenga09032017@gmail.com>
2025-02-26 13:23:27 +0100

    complete: teach communicate menu screen Done

---
+++ components/communicate/Communicate.tsx
@@ -0,0 +1,200 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import PageTitleH1 from "@/components/ui/page-title-h1";
+import { ComposeMessageDialog } from "./ComposeMessageDialog";
+import { generalImages, teacherImages } from "@/constants/images";
+import { ExternalCommunicationDialog } from "./ExternalCommunicationDialog";
+import { useState } from "react";
+import Image from "next/image";
+import CommunicationItem from "./CommunicationItem";
+import { Communication } from "@/types";
+
+type CommunicationGroupsWrapper = {
+  id: number;
+  title: string;
+  iconUrl: string;
+  communications: Communication[];
+};
+
+const communicationGroups: CommunicationGroupsWrapper[] = [
+  {
+    id: 0,
+    title: "INDIVIDUAL STUDENT",
+    iconUrl: teacherImages.individual_student,
+    communications: [
+      {
+        userImageUrl: `${generalImages.student}`,
+        userName: "Patrick Jane",
+        message: "Hi Teacher, just to know, how is health now ?",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "WHOLE CLASS",
+    iconUrl: teacherImages.whole_class,
+    communications: [
+      {
+        userImageUrl: `${generalImages.student2}`,
+        userName: "Merry Jones",
+        message:
+          "Hi Teacher, Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia rem aperiam dicta excepturi molestias quasi repellendus ut. Tenetur, tempore!",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+      {
+        userImageUrl: `${generalImages.student}`,
+        userName: "Clara Morgan",
+        message:
+          "Hello, Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia rem aperiam dicta excepturi molestias quasi repellendus ut. Tenetur, tempore!",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "INDIVIDUAL PARENT",
+    iconUrl: teacherImages.individual_parent,
+    communications: [
+      {
+        userImageUrl: `${generalImages.student2}`,
+        userName: "Merry Jones",
+        message:
+          "Hi Teacher, Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia rem aperiam dicta excepturi molestias quasi repellendus ut. Tenetur, tempore!",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+      {
+        userImageUrl: `${generalImages.student}`,
+        userName: "Clara Morgan",
+        message:
+          "Hello, Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia rem aperiam dicta excepturi molestias quasi repellendus ut. Tenetur, tempore!",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "ALL PARENTS",
+    iconUrl: teacherImages.all_parents,
+    communications: [
+      {
+        userImageUrl: `${generalImages.student2}`,
+        userName: "Merry Jones",
+        message:
+          "Hi Teacher, Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia rem aperiam dicta excepturi molestias quasi repellendus ut. Tenetur, tempore!",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+      {
+        userImageUrl: `${generalImages.student}`,
+        userName: "Clara Morgan",
+        message:
+          "Hello, Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia rem aperiam dicta excepturi molestias quasi repellendus ut. Tenetur, tempore!",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "OTHER GROUPS",
+    iconUrl: teacherImages.whole_class,
+    communications: [
+      {
+        userImageUrl: `${generalImages.student2}`,
+        userName: "Merry Jones",
+        message:
+          "Hi Teacher, Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia rem aperiam dicta excepturi molestias quasi repellendus ut. Tenetur, tempore!",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+      {
+        userImageUrl: `${generalImages.student}`,
+        userName: "Clara Morgan",
+        message:
+          "Hello, Lorem ipsum dolor sit, amet consectetur adipisicing elit. At officia rem aperiam dicta excepturi molestias quasi repellendus ut. Tenetur, tempore!",
+        created: "13:15 21/02/25",
+        attachedFilesUrls: [
+          { name: "My file 1", url: "/" },
+          { name: "My second file", url: "/" },
+        ],
+      },
+    ],
+  },
+];
+
+export default function Communicate() {
+  const [currentCommunicationGroup, setCurrentCommunicationGroup] =
+    useState<CommunicationGroupsWrapper>(communicationGroups[0]);
+
+  return (
+    <section className="flex flex-col gap-2 w-full p-6">
+      <div className="flex items-center">
+        <PageTitleH1 title="COMMUNICATE" />
+      </div>
+      <div className="flex gap-4 justify-start">
+        <ComposeMessageDialog />
+        <ExternalCommunicationDialog />
+      </div>
+      <Card className="rounded-md">
+        <div className="w-full flex gap-6 bg-[#dfecf1] p-4 pb-0 pl-0">
+          <div className="flex gap-4 pl-4 overflow-scroll">
+            {communicationGroups.map((communicationGroup) => (
+              <label
+                className={`text-[#55b4f1] whitespace-nowrap cursor-pointer p-2 flex flex-col items-center justify-between ${
+                  currentCommunicationGroup.id === communicationGroup.id
+                    ? "bg-white rounded-t-lg"
+                    : ""
+                }`}
+                onClick={() => setCurrentCommunicationGroup(communicationGroup)}
+              >
+                <Image
+                  src={communicationGroup.iconUrl}
+                  alt="Groug icon"
+                  width={20}
+                  height={20}
+                />{" "}
+                <span>{communicationGroup.title}</span>
+              </label>
+            ))}
+          </div>
+        </div>
+        <div className="p-4 overflow-scroll">
+          {currentCommunicationGroup.communications.map((communication) => (
+            <CommunicationItem communicationItem={communication} />
+          ))}
+        </div>
+      </Card>
+    </section>
+  );
+}
