41a696e4d8e6c8cef97d9c6c1f1365fce0b6d223
princebak <princebakenga09032017@gmail.com>
2025-03-25 13:42:37 +0100

    attendance  get all new API initiated

--- components/attendance/attendance-table.tsx
+++ components/attendance/attendance-table.tsx
@@ -11,17 +11,24 @@ import {
 import AttendanceItem from "../ui/attendance-item";
 import { getAttendances, updateAttendance } from "@/services/AttendanceService";
 import { Loader } from "../ui/loader";
-import { useList } from "@/hooks/useList";
 import { useRequestInfo } from "@/hooks/useRequestInfo";
 import NoData from "../ui/no-data";
+import { useEffect, useState } from "react";
+import { localStorageKey } from "@/constants/global";
 
 export function AttendanceTable({
   isAttendanceDatePast,
+  currentDate,
+  currentClassId,
 }: {
   isAttendanceDatePast: boolean;
+  currentDate: string;
+  currentClassId: number;
 }) {
-  const { list: attendances, isLoading, error } = useList(getAttendances);
-  const { accessToken, refreshToken, tenantDomain } = useRequestInfo();
+  const [attendances, setAttendances] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { tenantDomain, accessToken, refreshToken } = useRequestInfo();
+  const userData = JSON.parse(localStorage.getItem(localStorageKey.USER_DATA)!);
 
   const handleUpdateAttendance = async (attendance: any) => {
     if (accessToken && refreshToken && tenantDomain) {
@@ -35,13 +42,35 @@ export function AttendanceTable({
     }
   };
 
+  useEffect(() => {
+    const loadAttendances = async () => {
+      if (tenantDomain && accessToken && refreshToken) {
+        const data = await getAttendances(
+          {
+            date: currentDate,
+            class_id: currentClassId,
+            teacher_id: userData.owner_id,
+          },
+          tenantDomain,
+          accessToken,
+          refreshToken
+        );
+
+        console.log("ATTENDANCES LOADED => ", data);
+        setAttendances(data);
+      }
+    };
+
+    loadAttendances();
+  }, [tenantDomain, accessToken, refreshToken]);
+
   return (
     <div className="w-full overflow-auto">
       {isLoading ? (
         <Loader />
       ) : (
         <>
-          {attendances.lenght > 0 ? (
+          {attendances.length > 0 ? (
             <Table>
               <TableHeader>
                 <TableRow>
@@ -53,10 +82,10 @@ export function AttendanceTable({
               <TableBody>
                 {attendances.map((attendance: any, index: any) => (
                   <TableRow key={attendance.id}>
-                    <TableCell>{attendance.student}</TableCell>
+                    <TableCell>{attendance.student.user.username}</TableCell>
                     <TableCell>
                       <AttendanceItem
-                        label={attendance.status}
+                        label={attendance.status ? attendance.status : "absent"}
                         isAttendanceDatePast={isAttendanceDatePast}
                         handleChange={(status: string) =>
                           handleUpdateAttendance({
@@ -69,14 +98,12 @@ export function AttendanceTable({
                     <TableCell>
                       <div className="flex gap-8">
                         <span className="border-b-4 border-bgGreenLight2">
-                          {attendance.presentAttendanceCount}
+                          {0} {/* TODO : attendance.presentAttendanceCount */}
                         </span>
                         <span className="border-b-4 border-bgPinkLight2">
-                          {attendance.absentAttendanceCount}
+                          {0}
                         </span>
-                        <span className="border-b-4 border-bgYellow">
-                          {attendance.lateAttendanceCount}
-                        </span>
+                        <span className="border-b-4 border-bgYellow">{0}</span>
                       </div>
                     </TableCell>
                   </TableRow>
