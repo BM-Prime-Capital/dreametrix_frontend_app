2131f1246c0594c3d6b8a6386050c14ce0ee0c85
Henock BARAKAEL <barahenock@gmail.com>
2025-04-07 19:05:10 +0100

    issue on types question

--- components/types/question.ts
+++ components/types/question.ts
@@ -1,10 +1,14 @@
 export interface Question {
-    id: number;
-    type: string;
-    links: string[];
-    main_link: string;
-    standard: string;
-    difficulty: string;
-    page_count: number;
-    preview_urls: string[];
-  }
+  id: number;
+  type: string;
+  links: string[];
+  main_link: string;
+  standard: string;
+  difficulty: string;
+  page_count: number;
+  preview_urls: string[];
+  // Add the missing properties here
+  questionNumber: number;
+  correctAnswer: string;
+  pointValue: number;
+}
